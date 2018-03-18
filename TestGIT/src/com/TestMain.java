bf5e9955f309dc170883666ab274f96a74aef84f
suersehii1 <suersehii1@gmail.com>
2018-03-18 19:24:45 +0900

    push 01

--- TestGIT/src/com/TestMain.java
+++ TestGIT/src/com/TestMain.java
@@ -2,4 +2,12 @@ package com;
 
 public class TestMain {
 
+	public static void main(String args[]) {
+		
+		TestMain t = new TestMain();
+	}
+	
+	private void go() {
+		System.out.println("1234");
+	}
 }
